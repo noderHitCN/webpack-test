9afe14dbd77ae0e64449590558c8270e9027d775
ywj <ppaii123@outlook.com>
2016-07-11 12:01:34 +0800

    second time changes

---
+++ public/src/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Hello Wrold!!</h1>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
--- src/app.js
+++ src/app.js
@@ -4,12 +4,16 @@ import bodyParser from 'body-parser';
 var app = express();
 var router = Router();
 
-app.use(express.static())
+// app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 router.get('/', (req, res, next) => {
 	res.send('Hello World!');
+});
+
+router.get('/user', (req, res) => {
+	res.send('this is user page.');
 });
 
 app.use('/', router);
